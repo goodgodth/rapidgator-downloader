4d7abb4da158eeaa840c5a39bd4f63e35fa86bbc
arruk204 <arruk204@exten.postbox.in.th>
2020-09-23 22:14:44 +0700

    Add rapidgatorsingle.py for download single file, Edit rapidgatordl.py for easy debug and fill filename from original Uploader by dont use API

--- README.md
+++ README.md
@@ -1,7 +1,40 @@
-# rapidgator-downloader
-## How to run
+# Rapidgator-Downloader
+Download file from rapidgator with premium user
 
-python rapidgatordl.py {USERNAME-premiumuser} {PASSWORD} {SAVE TO DIRECTORY} {FULL PATH OF FILE dl.txt}
+## rapidgatordl.py
+Download by read list of URL in text file (Example : dl.txt)
 
-Example
-python rapidgatordl.py noeybnk48<span>@<span>gmail.com noeyBNK48 /home/noeybnk48/download/ /home/noeybnk48/dl.txt
+### How to run
+
+```sh
+$ python rapidgatordl.py {USERNAME-premiumuser} {PASSWORD} {FULL_PATH_OF_FILE_dl.txt} {FULL_PATH_OF_SAVE_TO_DIRECTORY}
+```
+
+### Example
+```sh
+$ python rapidgatordl.py noeybnk48@gmail.com noeyBNK48 /home/noeybnk48/dl.txt /home/noeybnk48/download/
+```
+
+
+## rapidgatorsingle.py
+Download by URL in argument
+
+### How to run
+
+```sh
+$ python rapidgatorsingle.py {USERNAME-premiumuser} {PASSWORD} {RAPIDURL} {FULL_PATH_OF_SAVE_TO_DIRECTORY}
+```
+
+### Example
+```sh
+$ python rapidgatorsingle.py noeybnk48@gmail.com noeyBNK48 https://rg.to/file/x54f6ed3db794b9b99ca14f6b70e053d /home/noeybnk48/download/
+```
+
+###
+
+---
+
+
+### Reference
+
+ - [Rapidgator API](https://rapidgator.net/article/api/index).
--- rapidgatordl.py
+++ rapidgatordl.py
@@ -1,60 +1,89 @@
-import requests
-import os
-import sys
+import requests , os , sys
+from lxml import html
 
-URL = "https://rapidgator.net/api/v2/user/login"
+RPGURL_LOGIN = "https://rapidgator.net/api/v2/user/login"
+#RPGURL_FILEINFO = "https://rapidgator.net/api/v2/file/info/"
+RPGURL_FILEDOWNLOAD = "https://rapidgator.net/api/v2/file/download/"
+RPG_XPATH_FILENAME = "//div[@class='text-block file-descr']//p//a//text()"
 
-PARAMS = {"login": sys.argv[1], "password": sys.argv[2]}
+if not len(sys.argv) == 5:
+    print ("Argument should be python rapidgatordl.py {USERNAME-premiumuser} {PASSWORD} {FULL PATH OF FILE dl.txt} {SAVE TO DIRECTORY}")
+    sys.exit(0)
 
-PATH = sys.argv[3]
+USERNAME = sys.argv[1]
+PASSWORD = sys.argv[2]
+PATH = sys.argv[4]
+LISTOFDOWNLOAD = sys.argv[3]
 
-r = requests.get(url=URL, params=PARAMS)
+
+#Validate
+if not os.path.exists(PATH) and os.path.isdir(PATH) :
+    print ("Save Directory not exist")
+    sys.exit(0)
+if not os.path.exists(LISTOFDOWNLOAD) and os.path.isfile(LISTOFDOWNLOAD) :
+    print ("List of download url not exist")
+    sys.exit(0)
+
+
+PARAMS = {"login": USERNAME, "password": PASSWORD}
+
+r = requests.get(url=RPGURL_LOGIN, params=PARAMS)
 data = r.json()
 print("data : " + str(data))
 token = data["response"]["token"]
-print("data : " + str(data) + "\ntoken : " + token)
 
-with open(sys.argv[4]) as fp:
+with open(LISTOFDOWNLOAD) as fp:
     line = fp.readline()
     cnt = 1
     while line:
         print("File {}: {}".format(cnt, line.strip()))
         file_name = None
         _url = line.strip()
-        _file_id = _url.split("/")[4]
-        try:
-            file_name = _url.split("/")[5]
-            file_name = file_name.replace(".html", "")
-            print("expect file name : " + file_name)
-        except:
-            print("file name not found")
-        url = "https://rapidgator.net/api/v2/file/info/"
-        params = {"file_id": _file_id, "token": token}
-        print("GET method TO URL : " + str(url) + " with param " + str(params))
-        r = requests.get(url=url, params=params)
-        data = r.json()
 
-        print("Response :" + str(data) + " \nFile name : ")
-        try:
-            file_name = data["response"]["link"]["file"]["name"]
-        except:
-            print("file name not found")
-        # print(
-        #     "File ID : "
-        #     + _file_id
-        #     + " -> "
-        #     + data["result"]["files"][_file_id]["status"]
-        # )
-        # if file_status == 200:
-        url = "https://rapidgator.net/api/v2/file/download/"
+        if not _url :
+            print("++END++")
+            sys.exit(0)
+
+        _file_id = _url.split("/")[4]
+
+        ## If want assign filename from URL (htttp://rg.to/408320/assign_filename_by_yourself.rar ==> file_name = assign_filename_by_yourself.rar
+        # try:
+        #     file_name = _url.split("/")[5]
+        #     file_name = file_name.replace(".html", "")
+        #     print("expect file name : " + file_name)
+        # except:
+        #     print("file name not found")
+        #     file_name = str(int(round(time.time() * 1000)))
+        ## end If
+
+        ## If want get filename from original uploader
+        page = requests.get(_url)
+        if page.status_code > 400 :
+            print("URL : "+_url+ "get HTTP STATUS "+str(page.status_code))
+            file_name = _file_id
+        else :
+            root = html.fromstring(page.text)
+            tree = root.getroottree()
+            result = root.xpath(RPG_XPATH_FILENAME)
+            file_name = ''.join([word.strip() for word in result])
+            file_name = len(file_name) > 0 and file_name or _file_id
+        ## end IF
+
+        url = RPGURL_FILEDOWNLOAD
         params = {"file_id": _file_id, "token": token}
         r = requests.get(url=url, params=params)
         data = r.json()
+        print(data)
         _dl_url = data["response"]["download_url"]
         print("Response :" + str(data) + " \ngetDownloadLink : " + str(_dl_url))
-        if file_name == None:
-            os.system("wget -P " + PATH + " -O " + _file_id + " '" + _dl_url + "'")
-        else:
-            os.system("wget -P " + PATH + " -O " + file_name + " '" + _dl_url + "'")
+        download_cmd = "wget -P " + PATH + " -O '" + file_name + "' '" + _dl_url + "'"
+
+        #Replace some special Chars in file_name
+        file_name = file_name.replace("&" , "_")
+        file_name = file_name.replace("(", "_")
+        file_name = file_name.replace(")", "_")
+
+        print(download_cmd)
+        os.system(download_cmd)
         line = fp.readline()
         cnt += 1
